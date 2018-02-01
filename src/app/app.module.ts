5bc9e067d3a0b4171d4a701c3e05f67bc44103ff
Anatolii Skoropupov <anatolii.skoropupov@gmail.com>
2018-02-01 19:00:55 +0200

    RouterModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AdminComponent } from './admin/admin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "home", component: HomeComponent },
+      { path: "products", component: ProductsComponent },
+      { path: "admin", component: AdminComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
